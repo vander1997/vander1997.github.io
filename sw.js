47e20c97d6fa1ef39bc5814b99cac2524862b923
vander1997 <47024714+vander1997@users.noreply.github.com>
2019-05-25 20:52:28 +0800

    1

--- sw.js
+++ sw.js
@@ -31,9 +31,9 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "www.mragain.site",
+  "www.mragain.site",
+  "www.mragain.site"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
